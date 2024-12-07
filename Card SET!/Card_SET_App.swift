f3cd6ae0e03f55e41ffa6e8e878214580307593e
GreenRamSilgen <kiranshrs@yahoo.com>
2024-12-06 18:45:09 -0800

    Initial Commit

---
+++ Card SET!/Card_SET_App.swift
@@ -0,0 +1,17 @@
+//
+//  Card_SET_App.swift
+//  Card SET!
+//
+//  Created by Kiran Shrestha on 12/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Card_SET_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Card SET!/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Card SET!
+//
+//  Created by Kiran Shrestha on 12/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
