944a2e7779c7bc7a6e465e16c661f76e79b55d80
GreenRamSilgen <kiranshrs@yahoo.com>
2024-12-24 17:11:49 -0800

    Initial commit. Created ShapeTesting to use for creating custom shapes with style and color as needed.

---
+++ Card SET!/ShapeTesting.swift
@@ -0,0 +1,18 @@
+//
+//  ShapeTesting.swift
+//  Card SET!
+//
+//  Created by Kiran Shrestha on 12/24/24.
+//
+
+import SwiftUI
+
+struct ShapeTesting: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShapeTesting()
+}
