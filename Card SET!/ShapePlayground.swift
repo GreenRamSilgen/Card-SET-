06baa101881ffd4c846b32d0517c9c9a407ae525
GreenRamSilgen <kiranshrs@yahoo.com>
2024-12-29 14:34:01 -0800

    Created Solid, Open and Striped Ellipses that can be customized based on inputs, for future use. Started on 'Squiggle' shape testing in the ShapePlayground using Shape and Path. **path.addQuadCurve used

---
+++ Card SET!/ShapePlayground.swift
@@ -0,0 +1,18 @@
+//
+//  ShapePlayground.swift
+//  Card SET!
+//
+//  Created by Kiran Shrestha on 12/29/24.
+//
+
+import SwiftUI
+
+struct ShapePlayground: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShapePlayground()
+}
